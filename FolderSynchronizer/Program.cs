485bfe63910ef8115a6bf92b8a285db73833b968
Bartek Tanski <bartitani@gmail.com>
2025-07-16 11:48:48 +0200

    add copying files and creating empty directories into replica folder

--- FolderSynchronizer/Program.cs
+++ FolderSynchronizer/Program.cs
@@ -27,39 +27,69 @@ class Program
             DisplayMessage($"Source directory: {sourceDirectory} doesn't exist!", ConsoleColor.Red);
             return;
         }
-        
+
         if (!Directory.Exists(replicaDirectory))
         {
             DisplayMessage($"Replica directory: {replicaDirectory} doesn't exist.", ConsoleColor.Yellow);
             DisplayMessage($"Creating directory at given path: {replicaDirectory} ...", ConsoleColor.DarkGray);
 
             Directory.CreateDirectory(replicaDirectory);
-            
+
             DisplayMessage("Directory created.");
         }
+
+        var filesInSourceDir = Directory.GetFiles(sourceDirectory);
+        var directoriesInSourceDir = Directory.GetDirectories(sourceDirectory);
+
+        foreach (var sourceFilePath in filesInSourceDir)
+        {
+            var targetFilePath = Path.Combine(replicaDirectory, Path.GetFileName(sourceFilePath));
+            
+            if (!File.Exists(targetFilePath))
+                CopyFile(sourceFilePath, targetFilePath);
+        }
+
+        foreach (var sourceDirectoryPath in directoriesInSourceDir)
+        {
+            var targetDirPath = Path.Combine(replicaDirectory, Path.GetFileName(sourceDirectoryPath));
+
+            if (!Directory.Exists(targetDirPath))
+                CreateMissingDirectories(sourceDirectoryPath, targetDirPath);
+        }
     }
-    
+
     private static void DisplayHelpMessage()
     {
         Console.WriteLine("Usage: FolderSynchronizer.exe <SourceDir> <ReplicaDir> <Interval> <LogPath>");
         Console.WriteLine("  Where:");
         Console.WriteLine("    <SourceDir>  - Path to source directory");
         Console.WriteLine("    <ReplicaDir> - Path to replica directory. All files must be the same as in source");
-        Console.WriteLine("    <Interval>   - Must be a number. Determines after what time content in folders is checked. ");
-        Console.WriteLine("    <LogPath>    - Path to directory where logs should be stored.");
+        Console.WriteLine("    <Interval>   - Must be a number. Synchronization interval");
+        Console.WriteLine("    <LogPath>    - Path to directory where logs should be stored");
 
         Console.WriteLine();
     }
-    
-    
+
     private static void DisplayMessage(string message, ConsoleColor? color = null)
     {
         var defaultConsoleColor = Console.ForegroundColor;
-        
+
         if (color is not null)
             Console.ForegroundColor = (ConsoleColor)color;
-        
+
         Console.WriteLine(message);
         Console.ForegroundColor = defaultConsoleColor;
+    }
+
+    private static void CopyFile(string sourcePath, string targetPath)
+    {
+        DisplayMessage($"Copying: {sourcePath} to {targetPath}", ConsoleColor.DarkGray);
+        File.Copy(sourcePath, targetPath);
+    }
+    
+    private static void CreateMissingDirectories(string sourcePath, string targetPath)
+    {
+        DisplayMessage($"Copying: {sourcePath} to {targetPath}", ConsoleColor.DarkGray);
+        Directory.CreateDirectory(targetPath);
     }
 }
