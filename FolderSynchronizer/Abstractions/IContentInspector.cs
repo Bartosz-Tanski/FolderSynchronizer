d17ad3bf68d3a2fcfe249cd8be1def767b850f63
Bartek Tanski <bartitani@gmail.com>
2025-07-22 11:53:03 +0200

    add method for checking directories names

--- FolderSynchronizer/Abstractions/IContentInspector.cs
+++ FolderSynchronizer/Abstractions/IContentInspector.cs
@@ -5,6 +5,7 @@ public interface IContentInspector
     bool HasSameContentSizes(string sourcePath, string replicaPath, out List<string> notValidReplicaFiles);
     bool IsContentIntegral(string sourcePath, string replicaPath, out List<string> notValidReplicaFiles);
     bool HasSameFilesNames(string sourcePath, string replicaPath, out List<string> notValidReplicaFiles);
+    bool HasSameDirectoriesNames(string sourcePath, string replicaPath, out List<string> notValidReplicaFiles);
     bool HasSameFileCount(string sourcePath, string replicaPath);
     bool HasSameDirectoryCount(string sourcePath, string replicaPath);
     bool DoesReplicaDirectoryExist(string replicaPath);
