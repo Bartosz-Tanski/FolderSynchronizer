d9700a7cb8d30b15b55081a7ff07dffdc00ea0f4
Bartek Tanski <bartitani@gmail.com>
2025-07-22 11:18:44 +0200

    rename HasSameContentNames name and remove comments

--- FolderSynchronizer/FileSystem/ContentInspector.cs
+++ FolderSynchronizer/FileSystem/ContentInspector.cs
@@ -13,23 +13,6 @@ public class ContentInspector : IContentInspector
         _contentManager = contentManager;
     }
 
-    // public bool HasSameContentSizes(string sourcePath, string replicaPath)
-    // {
-    //     var allSourceFiles = _contentManager.GetAllFilesPaths(sourcePath);
-    //     var allReplicaFiles = _contentManager.GetAllFilesPaths(replicaPath);
-    //
-    //     for (int i = 0; i < allSourceFiles.Length; i++)
-    //     {
-    //         var sourceFileInfo = new FileInfo(allSourceFiles[i]);
-    //         var replicaFileInfo = new FileInfo(allReplicaFiles[i]);
-    //
-    //         if (sourceFileInfo.Length != replicaFileInfo.Length)
-    //             return false;
-    //     }
-    //
-    //     return true;
-    // }
-    //
     public bool HasSameFileCount(string sourcePath, string replicaPath)
     {
         var sourceFiles = _contentManager.GetAllFilesPaths(sourcePath);
@@ -45,46 +28,6 @@ public class ContentInspector : IContentInspector
     
         return sourceDirectories.Length == replicaDirectories.Length;
     }
-    //
-    // public bool HasSameContentNames(string sourcePath, string replicaPath)
-    // {
-    //     throw new NotImplementedException();
-    // }
-    //
-    //
-    // public bool IsContentIntegral(string sourcePath, string replicaPath)
-    // {
-    //     var allSourceFiles = _contentManager.GetAllFilesPaths(sourcePath);
-    //     var allReplicaFiles = _contentManager.GetAllFilesPaths(replicaPath);
-    //
-    //     for (int i = 0; i < allSourceFiles.Length; i++)
-    //     {
-    //         var sourceFileHash = ComputeMd5(allSourceFiles[i]);
-    //         var replicaFileHash = ComputeMd5(allReplicaFiles[i]);
-    //
-    //         if (!string.Equals(sourceFileHash, replicaFileHash))
-    //         {
-    //             return false;
-    //         }
-    //     }
-    //     
-    //     return true;
-    // }
-    //
-    // private string ComputeMd5(string filePath)
-    // {
-    //     using var md5 = MD5.Create();
-    //     using var stream = File.OpenRead(filePath);
-    //     
-    //     var hash = md5.ComputeHash(stream);
-    //     var stringBuilder = new StringBuilder(hash.Length * 2);
-    //     
-    //     foreach (var singleByte in hash)
-    //         stringBuilder.Append(singleByte.ToString("x2"));
-    //     
-    //     return stringBuilder.ToString();
-    // }
-
 
     public bool HasSameContentSizes(string sourcePath, string replicaPath, out List<string> notValidReplicaFiles)
     {
@@ -148,7 +91,7 @@ public class ContentInspector : IContentInspector
         return stringBuilder.ToString();
     }
 
-    public bool HasSameContentNames(string sourcePath, string replicaPath, out List<string> notValidReplicaFiles)
+    public bool HasSameFilesNames(string sourcePath, string replicaPath, out List<string> notValidReplicaFiles)
     {
         var result = true;
         
