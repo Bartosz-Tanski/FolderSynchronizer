3629d29e0745bf59858c7171b244148cdc6be188
Bartek Tanski <bartitani@gmail.com>
2025-07-23 09:55:29 +0200

    create class for configuring logger

---
+++ FolderSynchronizer/Logger/LoggerConfigurator.cs
@@ -0,0 +1,22 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace FolderSynchronizer.Logger;
+
+public static class LoggerConfigurator
+{
+    public static void Configure(string logFilePath)
+    {
+        const string fileOutputTemplate =
+            "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}{NewLine}";
+        
+        const string consoleOutputTemplate =
+            "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}";
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .WriteTo.Console(outputTemplate: consoleOutputTemplate)
+            .WriteTo.File(logFilePath, restrictedToMinimumLevel: LogEventLevel.Information, outputTemplate: fileOutputTemplate)
+            .CreateLogger();
+    }
+}
