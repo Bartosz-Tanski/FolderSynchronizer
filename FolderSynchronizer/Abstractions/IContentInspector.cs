eb7604210a412a46935da4b6fd037d0198ffc651
Bartek Tanski <bartitani@gmail.com>
2025-07-23 08:58:15 +0200

    remove code duplication

--- FolderSynchronizer/Abstractions/IContentInspector.cs
+++ FolderSynchronizer/Abstractions/IContentInspector.cs
@@ -3,10 +3,8 @@
 public interface IContentInspector
 {
     bool DoesReplicaDirectoryExist(string replicaPath);
-    bool HasSameDirectoryCount(string sourcePath, string replicaPath);
-    bool HasSameFileCount(string sourcePath, string replicaPath);
-    bool HasSameContentSizes(string sourcePath, string replicaPath, out List<string> notValidReplicaFiles);
-    bool HasSameFilesNames(string sourcePath, string replicaPath, out List<string> notValidReplicaFiles);
-    bool HasSameDirectoriesNames(string sourcePath, string replicaPath);
-    bool IsContentIntegral(string sourcePath, string replicaPath, out List<string> notValidReplicaFiles);
+    bool HasSameCount(string[] sourceContent, string[] replicaContent);
+    bool HasSameFileSizes(string[] sourceFiles, string[] replicaFiles, out List<string> notValidReplicaFiles);
+    bool HasSameNames(string[] sourceContent, string[] replicaContent, out List<string> notValidReplicaFiles);
+    bool IsContentIntegral(string[] sourceFiles, string[] replicaFiles, out List<string> notValidReplicaFiles);
 }
